e0569a4bdfdfda2b14aae51fddbdcb56e58e1710
pepinho24 <p_mil4ev@hotmail.com>
2014-11-08 01:34:38 +0200

    Added Index pages for areas and Links to them in Home page

---
+++ MotivateMe/MotivateMe.Web/Areas/Forum/Controllers/ForumController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MotivateMe.Web.Areas.Forum.Controllers
+{
+    public class ForumController : Controller
+    {
+        // GET: Forum/Forum
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MotivateMe/MotivateMe.Web/Areas/Stories/Controllers/StoriesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MotivateMe.Web.Areas.Stories.Controllers
+{
+    public class StoriesController : Controller
+    {
+        // GET: Stories/Stories
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
