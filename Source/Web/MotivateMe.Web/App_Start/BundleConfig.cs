3391e306a058153007e7f787cc30d6588d24df7a
pepinho24 <p_mil4ev@hotmail.com>
2014-11-10 23:26:05 +0200

    Installed Kendo UI for MVC5

--- Source/Web/MotivateMe.Web/App_Start/BundleConfig.cs
+++ Source/Web/MotivateMe.Web/App_Start/BundleConfig.cs
@@ -8,8 +8,38 @@ namespace MotivateMe.Web
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            bundles.IgnoreList.Clear();
+
+            RegisterStyleBundles(bundles);
+            RegisterScryptBundles(bundles);
+
+            // Set EnableOptimizations to false for debugging. For more information,
+            // visit http://go.microsoft.com/fwlink/?LinkId=301862
+            BundleTable.EnableOptimizations = false;
+        }
+
+        private static void RegisterStyleBundles(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                        "~/Content/kendo/kendo.common-bootstrap.min.css",
+                        "~/Content/kendo/kendo.blueopal.min.css"));
+           
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap.cerulean.css",
+                      "~/Content/site.css"));
+        }
+
+        private static void RegisterScryptBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/kendo") .Include(
+                        "~/Scripts/kendo/kendo.all.min.js",
+                         "~/Scripts/kendo/kendo.aspnetmvc.min.js"));    
+
+            bundles.Add(new ScriptBundle("~/bundles/jquery")
+            .Include("~/Scripts/kendo/jquery.min.js"));    
+            //.Include( "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -22,15 +52,6 @@ namespace MotivateMe.Web
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
-
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/bootstrap.cerulean.css",
-                      "~/Content/site.css"));
-
-            // Set EnableOptimizations to false for debugging. For more information,
-            // visit http://go.microsoft.com/fwlink/?LinkId=301862
-            BundleTable.EnableOptimizations = true;
         }
     }
 }
