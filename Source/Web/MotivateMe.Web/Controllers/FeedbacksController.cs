ec071f20bb1200582172678aca44af949be2e933
pepinho24 <p_mil4ev@hotmail.com>
2014-11-18 04:28:43 +0200

    Added untracked controllers

---
+++ Source/Web/MotivateMe.Web/Controllers/FeedbacksController.cs
@@ -0,0 +1,56 @@
+﻿namespace MotivateMe.Web.Controllers
+{
+    using MotivateMe.Data;
+    using MotivateMe.Data.Models;
+    using MotivateMe.Web.ViewModels.Home;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using System.Web.Mvc;
+
+    public class FeedbacksController : BaseController
+    {
+        public FeedbacksController(IMotivateMeData data)
+            : base(data)
+        {
+            
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            var model = new CreateFeedbackInputModel();
+            return RedirectToAction("Contact", "Home", model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(CreateFeedbackInputModel input)
+        {
+            if (ModelState.IsValid)
+            {
+                var feedback = new Feedback
+                {
+                    Title = input.Title,
+                    Content = input.Content,
+                };
+
+                if (this.User.Identity.IsAuthenticated)
+                {
+                    var userId = this.CurrentUser.Id;
+                    feedback.AuthorId = userId;
+                }
+
+                this.Data.Feedbacks.Add(feedback);
+                this.Data.SaveChanges();
+
+                TempData["SuccessMessage"] = "Your feedback was sent successfully";
+
+                return this.RedirectToAction("Index", "Home");
+            }
+            TempData["ErrorMessage"] = "Something went wrong ;(";
+            return RedirectToAction("Contact", "Home", input);
+        }
+    }
+}
---
+++ Source/Web/MotivateMe.Web/ViewModels/Home/CreateFeedbackInputModel.cs
@@ -0,0 +1,17 @@
+﻿namespace MotivateMe.Web.ViewModels.Home
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class CreateFeedbackInputModel
+    {
+        [Required]
+        [Display(Name = "Title")]
+        [MaxLength(20)]
+        public string Title { get; set; }
+
+        [Required]
+        [Display(Name = "Content")]
+        [MaxLength(2500)]
+        public string Content { get; set; }
+    }
+}
